ae464be77879416fbd0233de7185e4c2d1418271
MonaJacob359 <monayagoub359@gmail.com>
2025-04-17 16:03:46 -0700

    update about

--- frontendbook/src/components/About.jsx
+++ frontendbook/src/components/About.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
       <section className="mb-12 text-center mt-16 md:w-[70%] m-auto">
         <h2 className="text-3xl font-bold mb-6 text-gray-900/70">About Us</h2>
         <p className="text-lg text-gray-700">
-          Welcome to [<span className='font-semibold text-teal-800'>Mahama BookStor</span>], your one-stop destination for all things books! Whether you're an avid reader, a casual book lover, or someone looking for that perfect gift, we have something for everyone. Our mission is to bring the joy of reading to people of all ages, backgrounds, and interests.
+          Welcome to [<span className='font-semibold text-teal-800'>Mahama BookStore</span>], your one-stop destination for all things books! Whether you're an avid reader, a casual book lover, or someone looking for that perfect gift, we have something for everyone. Our mission is to bring the joy of reading to people of all ages, backgrounds, and interests.
         </p>
       </section>
 
